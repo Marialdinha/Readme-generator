15465eae1c8c5f287b4a95cccc2f6c14e837e0b1
Marialdinha <marialda@bellsouth.net>
2023-06-20 18:46:25 -0500

    Added function that captures prompt input

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +8,23 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt([
+    {
+      type: 'password',
+      message: 'Enter a password',
+      name: 'password1',
+    },
+    {
+      type: 'password',
+      message: 'Enter a masked password',
+      name: 'password2',
+      mask: '*',
+    },
+  ])
+  .then((answers) => console.log(JSON.stringify(answers, null, '  ')));
+}
 
 // Function call to initialize app
 init();
